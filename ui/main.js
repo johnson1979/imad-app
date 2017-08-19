b985e7d8a9f5827d71c9d737d345dd08185e2557
johnson1979 <john7on.g@gmail.com>
2017-08-19 11:51:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getElementById("main-text");
-element.innerHtml="hey im changed";
+var element=document.getElementById('main-text');
+element.innerHTML="hey im changed";
