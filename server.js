b2713ec4980ef5ae305d2662b69ddd355aec9fe3
johnson1979 <john7on.g@gmail.com>
2017-08-19 07:39:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+res.send('article-one will be server here..!');
+});
+app.get('/article-two',function(req,res){
+res.send('article-two will be server here..!');
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
