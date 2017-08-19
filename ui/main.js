eb8e5991f2a5840738f047b94fd40d81f8c80fe0
johnson1979 <john7on.g@gmail.com>
2017-08-19 12:18:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ var element=document.getElementById('main-text');
 element.innerHTML='hey im changed';
 var img=document.getElementById('madi');
 
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 img.onclick= function() {
-    img.style.marginLeft= '100px';
+    var interval=setInterval(moveRight,50);
 };
