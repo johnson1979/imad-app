6ac5a4d34d0437f547629914468a57c9bcf2da79
johnson1979 <john7on.g@gmail.com>
2017-08-26 09:32:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
@@ -37,6 +38,17 @@ app.get('/test-db',function(req,res){
     }
 
 });
+});
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input',function(req,res){
+   var hashedString=hash(req.params.input,'this-is-a-random-string');
+   res.send(hashedString);
+    
 });
 
 app.get('/article-one',function(req,res){
